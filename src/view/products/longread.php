03e84e23a4b73f9e059731210b1700cf3eab8050
liencard <lien.cardoen@student.howest.be>
2020-01-18 23:59:21 +0100

    finalised coding all content to html-css #20

--- src/view/products/longread.php
+++ src/view/products/longread.php
@@ -11,7 +11,7 @@
         <source media="(max-width: 450px)" srcset="./assets/beelden/header-mobile.png 375w" />
         <source media="(max-width: 800px)" srcset="./assets/beelden/header-tablet.png 1194w" />
         <source media="(min-width: 1400px)" srcset="./assets/beelden/header-desktop.png 1438w" />
-        <img src="assets/beelden/header-tablet.png" alt="The Road header image">
+        <img class="header__image" src="assets/beelden/header-tablet.png" alt="The Road header image">
       </picture>
     </div>
 
@@ -66,11 +66,75 @@
   </section>
 
   <section class="part morality">
-    <h2 class="part__title ">ieder voor zichzelf</h2>
-    <div class="clock-text__wrapper">
+    <h2 class="part__title morality__title">Ieder voor <br> zichzelf</h2>
+    <div class="morality-text__wrapper">
       <p class="part__text">McCarthy beweert dat <span class="text__highlight">de samenleving de morele kompassen van de mensen dicteert.</span> Zonder deze maatschappelijke normen, zijn hun persoonlijke normen en moralen niet te onderscheiden van hun eigenbelang.<p>
       <p class="part__text">In het boek zie je hoe, door de <span class="text__highlight">afwezigheid van de menselijke beschaving</span> en de sociale normen, iedereen uitsluitend <span class="text__highlight">handelt uit eigenbelang.</span> Iedereen is egoïstisch en gelooft steevast dat zijn of haar eigen daden correct en gerechtvaarigd zijn.<p>
       <p class="part__text">De man kijkt alleen naar zijn zoon en zichzelf. Hierdoor, ziet hij al de andere overlevers als hun vijanden. Volgens hem zijn zij “de goede” en al de rest zijn “de slechte”.<p>
     </div>
+    <div class="morality__img-wrapper">
+      <picture>
+        <!-- <source media="(max-width: 600px)" srcset="./assets/beelden/fallenworld-mobile.png 338w" /> -->
+        <source media="(min-width: 600px)" srcset="./assets/beelden/paint-spot.png 819w" />
+        <img src="assets/beelden/paint-spot.png" class="morality__img" alt="Paint spot with excerpt">
+      </picture>
+    </div>
   </section>
+
+  <section class="part goodbad">
+    <h2 class="part__title goodbad__title">De goede <br> versus <br> de slechte</h2>
+    <p class="part__text">Volgens The Road, wat houd het in om een van de “goede” te zijn? In het boek is er één duidelijke regel wat de goede van de slechte onderscheid: <span class="text__highlight">De slechte eten mensen, de goede niet.</span><p>
+    <p class="part__text">Dit is echter niet het enige principe waaraan “de goede” moeten voldoen.<p>
+  </section>
+
+  <section class="part struggle">
+    <h2 class="part__title struggle__title">Innerlijk strijd</h2>
+    <p class="part__text">Doorheen het verhaal zie je de strijd van de man om één van “de goed” te blijven, om te doen wat goed is, in een wereld waarin de meeste mensen hun moralen volledig hebben achtergelaten.<p>
+    <p class="part__text">Nullam dictum turpis in maximus malesuada. Nam mollis, felis ut condimentum convallis, nisl diam tincidunt nibh, at semper tortor.<p>
+  </section>
+
+  <section class="part awards">
+    <h2 class="part__title awards__title">Rijkelijk <br> bekroond <br> boek</h2>
+
+    <div class="awards__text-wrapper">
+      <div class="awards__wrapper">
+        <p class="award_07">The Road wers bekroond met de <span class="text__highlight">Pulitzer Price for Fiction</span></p>
+        <p class="award_08">Verkozen door Entertainment Weekly als één van de <span class="text__highlight">‘beste boeken over de voorbije 25 jaar’</span></p>
+      </div>
+
+      <h3 class="awards__subtitle">The Road in 2019</h3>
+      <p class="part__text">Zelf vorig jaar, in 2019, werd het boek meerdere malen genomineerd op verschillende lijsten. Het boek boek heeft duidelijk, tot op vandaag, nog steeds een hoog aanzien. <p>
+      <ul class="awards__list">
+        <li class="award__item">BBC News vernoemde The Road in hun lijst van <span class="text__highlight">‘100 meest invloedrijke romans’.</span></li>
+        <li class="award__item item__image">The Guardian rangschikte The Road 17e op de lijst van <span class="text__highlight">‘100 beste boeken van de 21ste eeuw’.</span></li>
+      </ul>
+    </div>
+    <img class="marker__img" srcset="./assets/beelden/markers.png" sizes="15px" src="./assets/beelden/markers.png" alt="Markers">
+  </section>
+
+  <section class="part promo">
+    <div class="promo__wrapper">
+      <h2 class="part__title promo__title">Call to <br> action</h2>
+      <p class="part__text">The Road is  <span class="text__highlight">meer dan gewoon een verhaal over een post- apocalyptische wereld.</span> Het gaat over de liefde tussen vader en zoon. Het gaat over de moed om je moralen te behouden in onmenselijke omstandigheden.<p>
+      <p class="part__text">The Road is een somber verhaal over een een stervende planeet. Het zal je laten nadenken over het lot van de menselijkheid. Maar als je het nieuws op zet hoor je niet veel beter.<p>
+      <div class="button__img-wrapper">
+        <img srcset="./assets/beelden/button.png 326w,
+              ./assets/beelden/button.png 753w"
+          sizes="50vw"
+          class="button__img" src="./assets/beelden/button.png" alt="Buy book button">
+      </div>
+    </div>
+    <div class="promo__img-wrapper">
+      <img srcset="./assets/beelden/book-mini.png 326w,
+            ./assets/beelden/book-big.png 753w"
+        sizes="100vw"
+        class="promo__img" src="./assets/beelden/book-big.png" alt="Promo book The Road">
+    </div>
+  </section>
+
+  <section class="part footer">
+    <h2 class="hidden">Footer</h2>
+    <p class="footer__text">mogelijk gemaakt door</p>
+  </section>
+
 </div>
